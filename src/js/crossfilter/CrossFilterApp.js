bb23b87b42359f4cae5056ba06127ca4c7c73d96
William Moore <w.moore@dundee.ac.uk>
2019-08-22 23:38:04 +0100

    Load csv file from omero_table to crossfilter

--- src/js/crossfilter/CrossFilterApp.js
+++ src/js/crossfilter/CrossFilterApp.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import config from '../config';
-import crossfilter from 'crossfilter2';
+import { DataContext } from './DataContext';
 
 class CrossFilterApp extends React.Component {
 
@@ -9,39 +8,13 @@ class CrossFilterApp extends React.Component {
         console.log('props', props);
     }
 
-    componentDidMount() {
-        console.log('componentDidMount...', config);
-
-        let url = config.webgatewayBaseUrl + 'table/Project/3060/query/?query=*';  //Image-18236';
-        fetch(url).then(rsp => rsp.json())
-            .then(data => {
-                console.log('d', data);
-                let cols = data.data.columns;
-                // map each row (list of values) into {colname:value} object
-                let tableData = data.data.rows.map(r => {
-                    let row = {};
-                    r.forEach((value, idx) => {
-                        row[cols[idx]] = value;
-                    });
-                    return row;
-                });
-                console.log('tableData', tableData);
-
-                var cf = crossfilter(tableData);
-
-                console.log(cf.size());
-
-                var countDimension = cf.dimension(function(d) {return d.Count;});
-
-                console.table(countDimension.top(5));
-            });
-    }
-
     render() {
         return (
-            <div className="columnContainer">
-                CrossFilter...
-            </div>
+            <DataContext>
+                <div className="columnContainer">
+                    CrossFilter...
+                </div>
+            </DataContext>
         )
     }
 }
