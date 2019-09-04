95c87cfd0e8f55196415177a7fa68ca6909aac9b
William Moore <w.moore@dundee.ac.uk>
2019-09-04 21:25:56 +0100

    PlotTemplate used internally by ScatterPlot

--- src/js/crossfilter/filters/ScatterPlot.js
+++ src/js/crossfilter/filters/ScatterPlot.js
@@ -1,9 +1,8 @@
 import React from "react";
 import * as dc from "dc";
 import {scaleLinear } from "d3";
-import { ChartTemplate } from "./ChartTemplate";
-// import { numberFormat } from "./cxContext";
-// import crossfilter from "crossfilter2";
+import { css } from "glamor";
+import { CXContext } from "../DataContext";
 
 const scatterPlotFunction = (divRef, ndx, dimName) => {
 
@@ -29,11 +28,95 @@ const scatterPlotFunction = (divRef, ndx, dimName) => {
     return plot;
 }
 
-export const ScatterPlot = props => (
-    <ChartTemplate
-        chartFunction={scatterPlotFunction}
-        title={props.dimName}
-        dimName={props.dimName}
-        removeChart={props.removeChart}
-    />
-)
+
+const PlotTemplate = props => {
+    /*
+    We render the dc chart using an effect. We want to pass the chart as a prop after the dc call,
+    but there is nothing by default to trigger a re-render and the prop, by default would be undefined.
+    To solve this, we hold a state key and increment it after the effect ran. 
+    By passing the key to the parent div, we get a rerender once the chart is defined. 
+    */
+  const context = React.useContext(CXContext);
+  const [chart, updateChart] = React.useState(null);
+  const ndx = context.ndx;
+  const div = React.useRef(null);
+  React.useEffect(() => {
+    const newChart = props.chartFunction(div.current, ndx, props.dimName); // chartfunction takes the ref and does something with it
+
+    newChart.render();
+    updateChart(newChart);
+
+    // Specify how to clean up after this effect:
+    return () => {
+      newChart.dimension().dispose();
+      dc.redrawAll();
+    };
+  }, []); {/*Run this exactly once */}
+
+  const chartStyles  = css({
+    width:'100%',
+    height:'auto',
+    boxSizing:'border-box',
+    padding: 10,
+    '& label':{
+      textTransform:'capitalize',
+      textDecoration:'underline'
+    }
+  })
+  return (
+    <div
+      ref={div}
+      {...chartStyles}
+    >
+    </div>
+  );
+};
+
+
+export const ScatterPlot = props => {
+
+    const context = React.useContext(CXContext);
+
+    const [xAxis, setXAxis] = React.useState()
+    const [yAxis, setYAxis] = React.useState()
+
+    const handleChangeX = (event) => {
+        let name = event.target.value;
+        setXAxis(name);
+    }
+    const handleChangeY = (event) => {
+        let name = event.target.value;
+        setYAxis(name);
+    }
+
+    const numberCols = context.columns.filter(col => col.type == 'number');
+
+    return (
+        <div>
+            <PlotTemplate
+                chartFunction={scatterPlotFunction}
+                title={props.dimName}
+                dimName={props.dimName}
+                removeChart={props.removeChart}
+            />
+            <div style={{'position': 'absolute', top: 10, right: 100}}>
+                <select onChange={handleChangeY} value={yAxis}>
+                    <option>Y axis</option>
+                    {numberCols.map(col => (
+                        <option value={col.name} key={col.name}>
+                            {col.name}
+                        </option>
+                    ))}
+                </select>
+                <select onChange={handleChangeX} value={xAxis}>
+                    <option>X axis</option>
+                    {numberCols.map(col => (
+                        <option value={col.name} key={col.name}>
+                            {col.name}
+                        </option>
+                    ))}
+                </select>
+            </div>
+        </div>
+    )
+}
